c2506e0113365ec0b907a724a5a9be267bc1b5c4
kongdej <kongdejs@gmail.com>
2017-02-04 13:14:50 +0700

    no message

--- main.js
+++ main.js
@@ -130,3 +130,14 @@
     });
 
     
+$('.timepicker').timepicker({
+    timeFormat: 'h:mm p',
+    interval: 30,
+//    minTime: '10',
+//    maxTime: '6:00pm',
+//    defaultTime: '11',
+    startTime: '08:00',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true
+});
