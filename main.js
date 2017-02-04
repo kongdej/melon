94bfa24ceb933f0f235bd9d5952679ba9d63878a
kongdej <kongdejs@gmail.com>
2017-02-04 14:28:56 +0700

    no message

--- main.js
+++ main.js
@@ -130,17 +130,4 @@
         microgear.publish("/set/wv",$('#wv').val());
     });
 
-    
-$('.timepicker').timepicker({
-    timeFormat: 'h:mm p',
-    interval: 30,
-//    minTime: '10',
-//    maxTime: '6:00pm',
-//    defaultTime: '11',
-    startTime: '08:00',
-    dynamic: true,
-    dropdown: true,
-    scrollbar: true
-});
-
 $('.clockpicker').clockpicker();
